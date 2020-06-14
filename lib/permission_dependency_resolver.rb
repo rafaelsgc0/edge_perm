5867b4c2a863bf272c18541e07f71502ae2da7d1
rafael carneiro <rafaelcarneiro@MacBook-Pro.local>
2020-06-14 18:33:34 +0100

    adding the files from the zip

---
+++ lib/permission_dependency_resolver.rb
@@ -0,0 +1,19 @@
+require 'tsort' # HINT
+
+class PermissionDependencyResolver
+  def initialize(dependencies)
+
+  end
+
+  def can_grant?(existing, perm_to_be_granted)
+
+  end
+
+  def can_deny?(existing, perm_to_be_denied)
+
+  end
+
+  def sort(permissions)
+
+  end
+end
